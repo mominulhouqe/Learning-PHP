ab33181fe393d54a40c2a938656a5679cd9f8ce4
mominul <ulhouqemomin39@gmail.com>
2024-01-14 11:47:47 +0600

    Added print and echo comparesion output

---
+++ print.php
@@ -0,0 +1,17 @@
+<?php
+
+$price=234;
+$quantity=32;
+$totalPrice=$price*$quantity;
+
+
+print"</br> Product  Price".$price."";
+print"</br> Total Quantity".$quantity;
+
+
+
+print"</br> Total Price".$totalPrice."";
+// print ekadik variable accept kore na
+
+
+?>
