e95c2849bd0a82c7faee6486e559bc001e0085b2
mominul <ulhouqemomin39@gmail.com>
2024-01-14 12:29:51 +0600

    Added data types

---
+++ datatypes.php
@@ -0,0 +1,49 @@
+<?php
+
+// 8 kind of data types in php
+/* 
+1.Number types
+2.String 
+3.boolean
+4.float
+5.Array
+6.Null
+7.Resource
+8.object
+
+*/
+
+// Example of datatypes 
+// 1.Number types
+
+$price = 1245;
+echo "</br> Price = " . $price;
+// 2.String
+$address = " Dhaka , Banagladesh";
+echo "</br> My Address = " . $address;
+// 3.Boolean
+$isAdmin = true;
+echo "</br> He is a Admin = " . $isAdmin;
+// 4.float
+
+$cgpa=3.5;
+echo "</br> My Computer science result is = ". $cgpa;
+
+// Array
+$car = array("Volvo","BMW","Toyta","Motorcy",);
+var_dump($car);
+// var_dump er maddome amara kun type er data seta check korte parbo
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+?>
